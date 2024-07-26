d113b283ec87c9d6be0f706dd97dbe20c9255dd1
rafi andrea <rafiandrealesmana@gmail.com>
2024-07-26 19:40:16 +0700

    update router

--- src/router.jsx
+++ src/router.jsx
@@ -9,17 +9,20 @@ import ProductPage from "./pages/sub-pages/tables/ProductPage";
 import {
     createBrowserRouter,
     Route,
-    createRoutesFromElements
+    createRoutesFromElements,
+    Navigate
 } from "react-router-dom";
 
 export const router = createBrowserRouter(
     createRoutesFromElements(
         <Route path="/" element={<DashboardMainLayout />}>
+            <Route index element={<Navigate to="dashboard" />} />
             <Route path="dashboard" element={<Dashboard />} />
             <Route path="profile" element={<DashboardProfile />} />
             <Route path="tables" element={<DashboardTables />}>
-                <Route path="customers" element={<CustomerPage />} />
+                <Route index element={<Navigate to="products" />} />
                 <Route path="products" element={<ProductPage />} />
+                <Route path="customers" element={<CustomerPage />} />
             </Route>
             <Route path="notifications" element={<DashboardNotifications />} />
         </Route>
